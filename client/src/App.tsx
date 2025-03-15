9d98e1f7b1a41bb8bf5497b505f9b91613c574e5
Indranil Halder <indranilhalder.dev@gmail.com>
2025-03-15 12:55:15 +0530

    Initial project setup

---
+++ client/src/App.tsx
@@ -0,0 +1,10 @@
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-4xl">Jbook App</h1>
+      <button className="btn btn-primary">Click Me</button>
+    </div>
+  );
+};
+
+export default App;
