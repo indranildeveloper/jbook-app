1dbcaa84d57316315be40ecc20126589ff727b26
Indranil Halder <indranilhalder.dev@gmail.com>
2025-03-15 15:07:26 +0530

    Setup esbuild

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,51 @@
-const App = () => {
+import { FC, useState, useEffect, useRef } from "react";
+import * as esbuild from "esbuild-wasm";
+
+const App: FC = () => {
+  const [inputCode, setInputCode] = useState<string>("");
+  const [code, setCode] = useState<string>("");
+  const initializeEsbuildRef = useRef<boolean>(false);
+
+  const startService = async () => {
+    await esbuild.initialize({
+      worker: true,
+      wasmURL: "/esbuild.wasm",
+    });
+    initializeEsbuildRef.current = true;
+  };
+
+  useEffect(() => {
+    startService();
+  }, []);
+
+  const handleClick = async () => {
+    if (!initializeEsbuildRef.current) return;
+    const result = await esbuild.transform(inputCode, {
+      loader: "jsx",
+      target: "es2015",
+    });
+
+    console.log(result);
+    setCode(result.code);
+  };
+
   return (
-    <div>
-      <h1 className="text-4xl">Jbook App</h1>
-      <button className="btn btn-primary">Click Me</button>
+    <div className="p-5 m-5">
+      <textarea
+        name="code"
+        id="code"
+        className="textarea mb-4"
+        placeholder="Enter your code"
+        value={inputCode}
+        onChange={(e) => setInputCode(e.target.value)}
+      ></textarea>
+      <div className="mb-4">
+        <button onClick={handleClick} className="btn btn-primary">
+          Submit
+        </button>
+      </div>
+
+      <pre className="border h-64 w-64">{code}</pre>
     </div>
   );
 };
