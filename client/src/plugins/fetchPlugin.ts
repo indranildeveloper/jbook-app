3035ef4dbfaa3c9b3df49c17f4976294b967e31e
Indranil Halder <indranilhalder.dev@gmail.com>
2025-03-17 15:49:03 +0530

    Refactor: Create different OnLoad Functions for js and css files with code cleanup

--- client/src/plugins/fetchPlugin.ts
+++ client/src/plugins/fetchPlugin.ts
@@ -11,43 +11,58 @@ export const fetchPlugin = (inputCode: string) => {
     name: "fetch-plugin",
     setup(build: esbuild.PluginBuild) {
       // Load the contents of the file
+      build.onLoad({ filter: /(^index\.js$)/ }, (): esbuild.OnLoadResult => {
+        return {
+          loader: "jsx",
+          contents: inputCode,
+        };
+      });
+
+      build.onLoad({ filter: /.*/ }, async (args: esbuild.OnLoadArgs) => {
+        const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(
+          args.path
+        );
+
+        if (cachedResult) {
+          return cachedResult;
+        }
+      });
+
       build.onLoad(
-        { filter: /.*/ },
+        { filter: /.css$/ },
         async (args: esbuild.OnLoadArgs): Promise<esbuild.OnLoadResult> => {
-          if (args.path === "index.js") {
-            return {
-              loader: "jsx",
-              contents: inputCode,
-            };
-          }
-
-          // const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(
-          //   args.path
-          // );
-
-          // if (cachedResult) {
-          //   return cachedResult;
-          // }
-
           const { data, request } = await axios.get(args.path);
 
-          const fileType = args.path.match(/.css$/) ? "css" : "jsx";
           const escaped = data
             .replace(/\n/g, "")
             .replace(/"/g, '\\"')
             .replace(/'/g, "\\'");
-          const contents =
-            fileType === "css"
-              ? `
+          const contents = `
               const style = document.createElement("style");
               style.innerText = '${escaped}';
               document.head.appendChild(style);
-          `
-              : data;
+          `;
 
           const result: esbuild.OnLoadResult = {
             loader: "jsx",
             contents,
+            resolveDir: new URL("./", request.responseURL).pathname,
+          };
+
+          await fileCache.setItem(args.path, result);
+
+          return result;
+        }
+      );
+
+      build.onLoad(
+        { filter: /.*/ },
+        async (args: esbuild.OnLoadArgs): Promise<esbuild.OnLoadResult> => {
+          const { data, request } = await axios.get(args.path);
+
+          const result: esbuild.OnLoadResult = {
+            loader: "jsx",
+            contents: data,
             resolveDir: new URL("./", request.responseURL).pathname,
           };
 
