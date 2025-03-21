4658ecdd5a70e03788c4892b33998a07f72c62ad
Indranil Halder <indranilhalder.dev@gmail.com>
2025-03-21 22:24:47 +0530

    Async error handling

--- client/src/constants/htmlTemplate.ts
+++ client/src/constants/htmlTemplate.ts
@@ -1,19 +1,28 @@
 // TODO: Serve HTML from a different server endpoint
-export const htmlTemplate = `
-    <html>
+export const htmlTemplate = /*html*/ `
+  <html>
       <head>
         <title>HTML TEMPLATE</title>
       </head>
       <body>
         <div id="root"></div>
         <script>
+          const handleError = (error) => {
+            const root = document.querySelector("#root");
+                root.innerHTML = '<div style="color: red;"><h4>Runtime Error:</h4>' + error + '</div>';
+                console.error(error);
+          };
+
+          window.addEventListener("error", (event) => {
+            event.preventDefault();
+            handleError(event.error);
+          });
+          
           window.addEventListener("message", (event) => {
             try{
               eval(event.data);
             } catch (error) {
-              const root = document.querySelector("#root");
-              root.innerHTML = '<div style="color: red;"><h4>Runtime Error:</h4>' + error + '</div>';
-              console.error(error);
+              handleError(error);
             }
           }, false);
         </script>
