f4ae3a8e068cfb4458f7b8ae4ed001cea5eb97a3
Indranil Halder <indranilhalder.dev@gmail.com>
2025-03-31 22:02:28 +0530

    The cells routes in local-api

--- packages/local-api/src/index.ts
+++ packages/local-api/src/index.ts
@@ -1,6 +1,7 @@
 import * as path from "node:path";
 import express, { Application, Request, Response } from "express";
 import { createProxyMiddleware } from "http-proxy-middleware";
+import { createCellsRouter } from "./routes/cellsRoutes";
 
 export const serve = (
   port: number,
@@ -22,6 +23,8 @@ export const serve = (
     const packagePath: string = require.resolve("local-client/dist/index.html");
     app.use(express.static(path.dirname(packagePath)));
   }
+
+  app.use(createCellsRouter(filename, dir));
 
   return new Promise<void>((resolve, reject) => {
     app.listen(port, () => resolve()).on("error", reject);
---
+++ packages/local-api/src/routes/cellsRoutes.ts
@@ -0,0 +1,48 @@
+import fs from "fs/promises";
+import * as path from "node:path";
+import express, { Request, Response } from "express";
+
+interface ICell {
+  id: string;
+  content: string;
+  type: "text" | "code";
+}
+
+export const createCellsRouter = (fileName: string, dirname: string) => {
+  const router = express.Router();
+  router.use(express.json());
+
+  const fullPath = path.join(dirname, fileName);
+
+  router.get("/cells", async (req: Request, res: Response) => {
+    try {
+      // Read the file
+      const result = await fs.readFile(fullPath, { encoding: "utf-8" });
+      res.send(JSON.parse(result));
+    } catch (error: unknown) {
+      if (
+        error instanceof Error &&
+        "code" in error &&
+        error.code === "ENOENT"
+      ) {
+        // Add code to create a file and add default cells        // Add code to create a file and add default cells
+        await fs.writeFile(fullPath, "[]", "utf-8");
+        res.send([]);
+      } else {
+        throw error;
+      }
+    }
+  });
+
+  router.post("/cells", async (req: Request, res: Response) => {
+    // Take the list of cells from the req object
+    // Serialize the content
+    const { cells }: { cells: ICell[] } = req.body;
+    // Write the cells into the file
+    await fs.writeFile(fullPath, JSON.stringify(cells), "utf-8");
+
+    res.send({ status: "success" });
+  });
+
+  return router;
+};
